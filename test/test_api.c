cf524e37ffc9dc27406a6f9ad10b5eec0ba9affc
cryptolime <budroni.alessandro@gmail.com>
2023-07-31 14:53:01 +0400

    Fix seed in testing, remove old file.

--- test/test_api.c
+++ test/test_api.c
@@ -1,12 +1,12 @@
 
-#include <stdio.h>
 #include <api.h>
 #include "test_utils.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 int main() {
 
-    printf("Start test permutation composition and inversion.\n\n");
+    printf("Start test permutation sampling, composition and inversion.\n\n");
     fflush(stdout);
 
     // Fisher Yates non-constant time
@@ -14,9 +14,14 @@ int main() {
     uint64_t *b_seed = (uint64_t*)seed;
     perm_t p, pinv, id;
 
+    int prng_seed = get_seed();
+
+    // initialize PRNG
+    srand(prng_seed);
+
     for (long long int i = 0; i < N_ITERATIONS; ++i) {
         // sample permutation
-        *b_seed = (uint64_t)i;
+        *b_seed = (uint64_t) rand();
         perm_set_random(p, (uint8_t *) seed);
         if (validate_permutation(p) != EXIT_SUCCESS) {
             printf("FAILED - Invalid permutation\n");
