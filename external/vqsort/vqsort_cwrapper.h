26330ddaf4bb50696efb5a2f67eb40c7607e13a1
Isaac Andrés Canales Martínez <icanalesm0500@gmail.com>
2023-08-09 10:31:22 +0400

    vqsort: Remove unused functions from C wrapper

--- external/vqsort/vqsort_cwrapper.h
+++ external/vqsort/vqsort_cwrapper.h
@@ -4,23 +4,7 @@
 #include <cstddef>
 #include <cstdint>
 
-extern "C" void vqsort_avx2_u16_asc(uint16_t* data, size_t len);
-extern "C" void vqsort_avx2_u16_des(uint16_t* data, size_t len);
 extern "C" void vqsort_avx2_u32_asc(uint32_t* data, size_t len);
-extern "C" void vqsort_avx2_u32_des(uint32_t* data, size_t len);
 extern "C" void vqsort_avx2_u64_asc(uint64_t* data, size_t len);
-extern "C" void vqsort_avx2_u64_des(uint64_t* data, size_t len);
-extern "C" void vqsort_avx2_i16_asc(int16_t* data, size_t len);
-extern "C" void vqsort_avx2_i16_des(int16_t* data, size_t len);
-extern "C" void vqsort_avx2_i32_asc(int32_t* data, size_t len);
-extern "C" void vqsort_avx2_i32_des(int32_t* data, size_t len);
-extern "C" void vqsort_avx2_i64_asc(int64_t* data, size_t len);
-extern "C" void vqsort_avx2_i64_des(int64_t* data, size_t len);
-
-extern "C" void vqsort_avx2_float_asc(float* data, size_t len);
-extern "C" void vqsort_avx2_float_des(float* data, size_t len);
-
-extern "C" void vqsort_avx2_double_asc(double* data, size_t len);
-extern "C" void vqsort_avx2_double_des(double* data, size_t len);
 
 #endif //RANDOM_PERMUTATION_SAMPLING_VQSORT_CWRAPPER_H
