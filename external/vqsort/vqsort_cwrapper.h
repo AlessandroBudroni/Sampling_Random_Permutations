aac24fe03ecfe87e1e201591097f8f827625e54c
Lucas Perin <lucas.perin@tii.ae>
2023-08-09 17:30:36 +0400

    Fix sort tests

--- external/vqsort/vqsort_cwrapper.h
+++ external/vqsort/vqsort_cwrapper.h
@@ -1,12 +1,10 @@
 #ifndef RANDOM_PERMUTATION_SAMPLING_VQSORT_CWRAPPER_H
 #define RANDOM_PERMUTATION_SAMPLING_VQSORT_CWRAPPER_H
 
-extern "C" {
-#include <inttypes.h>
-#include <stddef.h>
+#include <cinttypes>
+#include <cstddef>
 
-void vqsort_avx2_u32_asc(uint32_t* data, size_t len);
-void vqsort_avx2_u64_asc(uint64_t* data, size_t len);
-};
+extern "C" void vqsort_avx2_u32_asc(uint32_t* data, size_t len);
+extern "C" void vqsort_avx2_u64_asc(uint64_t* data, size_t len);
 
 #endif //RANDOM_PERMUTATION_SAMPLING_VQSORT_CWRAPPER_H
--- test/test_sort_u64.c
+++ test/test_sort_u64.c
@@ -8,50 +8,19 @@
 extern void vqsort_avx2_u32_asc(uint32_t* data, size_t len);
 extern void vqsort_avx2_u64_asc(uint64_t* data, size_t len);
 
-void test_uint32_sort() {
-    uint32_t data_djb[PARAM_N] = {0};
-    uint32_t data_vqs[PARAM_N] = {0};
-    uint8_t seed[SEED_BYTES] = {0};
-
-    SHAKE128((uint8_t *)data_djb, sizeof(data_djb), seed, sizeof(seed));
-    memcpy(data_vqs, data_djb, sizeof(data_djb));
-
-    uint32_sort(data_djb, PARAM_N);
-    vqsort_avx2_u32_asc(data_vqs, PARAM_N);
-
-    uint32_t previous_djb = 0;
-    uint32_t previous_vqs = 0;
-    for (int i = 0; i < PARAM_N; i++) {
-        // Test djbsort ordering
-        if (data_djb[i] < previous_djb ) {
-            printf("djbsort32 no good..\n");
-            exit(EXIT_FAILURE);
-        }
-        previous_djb = data_djb[i];
-
-        // Test vqsort ordering
-        if (data_vqs[i] < previous_vqs ) {
-            printf("vqsort32 no good..\n");
-            exit(EXIT_FAILURE);
-        }
-        previous_vqs = data_vqs[i];
-
-        // Test compatibility
-        if (data_djb[i] != data_vqs[i]) {
-            printf("sorting 32 not compatible!\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-    printf("sort 32 ok!\n");
-}
-
-
-void test_uint64_sort() {
+int main() {
+    printf("Start sorting test with %s\n", scheme_name());
     uint64_t data_djb[PARAM_N] = {0};
     uint64_t data_vqs[PARAM_N] = {0};
     uint8_t seed[SEED_BYTES] = {0};
 
     SHAKE128((uint8_t *)data_djb, sizeof(data_djb), seed, sizeof(seed));
+#ifdef DJBSORT_IS_MCELIECE
+    printf("Using Mceliece Djbsort, sorting up to 63 bits\n");
+    for (int i =0; i < PARAM_N; ++i) {
+        data_djb[i] &= 0x7FFFFFFFFFFFFFFF;
+    }
+#endif
     memcpy(data_vqs, data_djb, sizeof(data_djb));
 
     uint64_sort(data_djb, PARAM_N);
@@ -59,6 +28,7 @@ void test_uint64_sort() {
 
     uint64_t previous_djb = 0;
     uint64_t previous_vqs = 0;
+
     for (int i = 0; i < PARAM_N; i++) {
         // Test djbsort ordering
         if (data_djb[i] < previous_djb ) {
@@ -79,9 +49,5 @@ void test_uint64_sort() {
         }
     }
     printf("sort 64 ok!\n");
-}
-
-int main() {
-    test_uint32_sort();
-    test_uint64_sort();
+    return EXIT_SUCCESS;
 }
