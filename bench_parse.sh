42f77973efa378f28b974341d79ce1c5c9206498
Isaac Andrés Canales Martínez <icanalesm0500@gmail.com>
2023-08-15 17:31:30 +0400

    benchmark: Parse benchmarks for uniform sampling

--- bench_parse.sh
+++ bench_parse.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
 
-# Usage: parse_bench operation [avx2]
+# Usage: parse_bench <sp, cp, ip> [avx2]
+#        parse_bench sui
 #
-# operation: Sample, Compose or Invert
+#        where sp  = sample permutations,
+#              cp  = compose permutations,
+#              ip  = invert permutations,
+#              sui = sample uniform integers
 
 PARAM_N="32 64 128 256 512 1024 2048"
 BENCH_DIR_MAIN="bench_dir"
@@ -13,41 +17,72 @@ BENCH_DIR_MAIN="bench_dir"
 # operation: Sample, Compose or Invert
 parseBenchmarkOutput()
 {
-	awk -v op="$1" '$0 ~ op {flag=1; next} /cycles/ {if (flag==1) {print $4; exit}}' "$2"
+	awk -v op="$1" '$0 ~ op {flag=1; next} /cycles/ {if (flag==1) {print $(NF-1); exit}}' "$2"
 }
+
+showUsage(){
+	printf "Usage: %s <sp|cp|ip> [avx2]\n" "$(basename "$0")"
+	printf "       %s sui\n\n" "$(basename "$0")"
+	printf "where sp  = sample permutations\n"
+	printf "      cp  = compose permutations\n"
+	printf "      ip  = invert permutations\n"
+	printf "      sui = sample uniform integers\n"
+}
+
+# ------------------------------------------------------------------------------
+# Parse command line arguments
+# ------------------------------------------------------------------------------
+
+if [ "$#" -lt 1 ]; then
+	showUsage
+	exit 1
+fi
+
+case "$1" in
+sp|sui)
+	op="Sample" ;;
+cp)
+	op="Compose" ;;
+ip)
+	op="Invert" ;;
+*)
+	showUsage
+	exit 1 ;;
+esac
+
+if [ -n "$2" ] && [ "$2" != "avx2" ]; then
+	showUsage
+	exit 1
+fi
 
 # ------------------------------------------------------------------------------
 # Main execution
 # ------------------------------------------------------------------------------
 
-if [ "$#" -lt 1 ]; then
-	printf "Usage: %s operation [avx2]\n\n" "$0"
-	printf "       operation = Sample | Compose | Invert\n"
-	exit 1
-fi
-
-op="$1"
-variant="$2"
-
-# Get list of schemes (from the first parameter N)
+# Get list of schemes (from the first parameter N) and datafile name
 n=$(echo "$PARAM_N" | awk '{print $1}')
 dir="$BENCH_DIR_MAIN/param_n_$n"
-case "$variant" in
-avx2)
-	schemes=$(ctest --test-dir "$dir" -N | awk '/benchmark_api/ && /avx2/ {print $3}')
-	variant="_$variant"
+case "$1" in
+sp|cp|ip)
+	if [ "$2" = "avx2" ]; then
+		schemes=$(ctest --test-dir "$dir" -N | awk '/benchmark_api/ && /avx2/ {print $3}')
+		variant="_$2"
+	else
+		schemes=$(ctest --test-dir "$dir" -N | awk '/benchmark_api/ && !/avx2/ {print $3}')
+		variant=""
+	fi
+	file="$BENCH_DIR_MAIN/benchmark_api_$op$variant.dat"
+	prefix="benchmark_api_"
 	;;
-*)
-	schemes=$(ctest --test-dir "$dir" -N | awk '/benchmark_api/ && !/avx2/ {print $3}')
-	variant=""
+sui)
+	schemes=$(ctest --test-dir "$dir" -N | awk '/benchmark_uniform_sampling/ {print $3}')
+	file="$BENCH_DIR_MAIN/benchmark_uniform_sampling.dat"
+	prefix="benchmark_uniform_sampling_"
 	;;
 esac
 
-# Data file
-file="$BENCH_DIR_MAIN/benchmark_api_$op$variant.dat"
 # Data file - header
 line="n"
-prefix="benchmark_api_"
 for scheme in $schemes; do
 	# Replace '_' for ' '
 	line="$line,$(echo "${scheme#"$prefix"}" | tr '_' ' ')"
